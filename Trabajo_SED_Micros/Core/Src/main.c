1d8420fb8b7067fe023b5f9be0d568fac808a4d7
Rachel g. f <r.gfranco@alumnos.upm.es>
2021-12-11 12:15:28 +0100

    Puerta garaje.
    Falta el revisar movimiento servo.

--- Trabajo_SED_Micros/Core/Src/main.c
+++ Trabajo_SED_Micros/Core/Src/main.c
@@ -41,6 +41,7 @@
 
 /* Private variables ---------------------------------------------------------*/
 TIM_HandleTypeDef htim1;
+TIM_HandleTypeDef htim2;
 
 /* USER CODE BEGIN PV */
 
@@ -50,6 +51,7 @@ TIM_HandleTypeDef htim1;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_TIM1_Init(void);
+static void MX_TIM2_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -68,6 +70,160 @@ uint32_t IC_Val2 = 0;
 uint32_t Difference = 0;
 uint8_t Is_First_Captured = 0;  // is the first value captured ?
 uint8_t Distance  = 0;
+
+//Variables puerta
+#define RANGE 11.1
+uint32_t temp_puerta, espera_puerta;
+int angulo=90, apertura=0, flag=0;
+float servo;
+volatile boton1=0,boton2=0,boton3=0,boton4=0;
+int luz=0;
+int modo=0;
+volatile uint16_t ADC_val=100;
+
+int debouncerBoton3(volatile int* button_int, GPIO_TypeDef* GPIO_port, uint16_t GPIO_number){
+	static uint8_t cuenta_boton=0;
+	static int cuenta=0;
+
+	if (*button_int==1){
+		if (cuenta_boton==0) {
+			cuenta=HAL_GetTick();
+			cuenta_boton++;
+		}
+		if (HAL_GetTick()-cuenta>=20){
+			cuenta=HAL_GetTick();
+			if (HAL_GPIO_ReadPin(GPIO_port, GPIO_number)!=1){
+				cuenta_boton=1;
+			}
+			else{
+				cuenta_boton++;
+			}
+			if (cuenta_boton==3){ //Periodo antirebotes
+				cuenta_boton=0;
+				*button_int=0;
+				return 1;
+			}
+		}
+
+	}
+	return 0;
+}
+
+int debouncerBoton4(volatile int* button_int, GPIO_TypeDef* GPIO_port, uint16_t GPIO_number){
+	static uint8_t cuenta_boton=0;
+	static int cuenta=0;
+
+	if (*button_int==1){
+		if (cuenta_boton==0) {
+			cuenta=HAL_GetTick();
+			cuenta_boton++;
+		}
+		if (HAL_GetTick()-cuenta>=20){
+			cuenta=HAL_GetTick();
+			if (HAL_GPIO_ReadPin(GPIO_port, GPIO_number)!=1){
+				cuenta_boton=1;
+			}
+			else{
+				cuenta_boton++;
+			}
+			if (cuenta_boton==3){ //Periodo antirebotes
+				cuenta_boton=0;
+				*button_int=0;
+				return 1;
+			}
+		}
+
+	}
+	return 0;
+}
+
+void garagecontrol(void)  //PUERTA
+{
+if((debouncerBoton3(&boton3,GPIOA,GPIO_PIN_0))==1)
+{
+ if(flag==1 && apertura==1)
+ {
+	 flag=0;
+   }
+ else if (flag==0 && apertura==1)
+ {
+	 flag=1;
+ }
+}
+if ((debouncerBoton4(&boton4,GPIOA,GPIO_PIN_1))==1)
+{
+ if(apertura==1)
+ {
+ apertura=0;
+espera_puerta=0;//---
+   }
+ else
+ {
+  apertura=1;
+ }
+ }
+ if(angulo==0 && flag==0)
+ {
+ if(espera_puerta==0 && apertura==1 )//_-
+	 espera_puerta = HAL_GetTick();
+ else if(HAL_GetTick()-espera_puerta >= 10000  &&  apertura==1)
+ {
+flag=1;
+espera_puerta=0;
+ }
+ }
+ if((HAL_GetTick()-temp_puerta )>=20 && apertura==1)
+ {
+ if(angulo<90 && flag==1)
+ {
+	 angulo=angulo+10;
+ espera_puerta=0;
+ temp_puerta=HAL_GetTick();
+ }
+ else if(angulo> 0 && flag==0)
+ {
+	 angulo=angulo-10;
+ espera_puerta=0;
+ temp_puerta=HAL_GetTick();
+ }
+ }
+ servo = (RANGE*angulo)+500;
+   htim2.Instance->CCR4 = servo;
+   HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, !apertura);
+}
+
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+if(GPIO_Pin == GPIO_PIN_0)
+{
+boton3=1;
+}
+if(GPIO_Pin == GPIO_PIN_1)
+{
+boton4=1;
+}
+if(GPIO_Pin == GPIO_PIN_15)
+{
+boton1=1;
+}
+if(GPIO_Pin == GPIO_PIN_14)
+{
+boton2=1;
+}
+if(GPIO_Pin==GPIO_PIN_3)
+{
+//if   (flag == 1) {flag = 0;}
+//else             {flag = 1;}
+}
+if(GPIO_Pin==GPIO_PIN_4)
+{
+//desactivar_alarma = 1;
+}
+}
+
+
+
 
 
 void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
@@ -148,8 +304,10 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_TIM1_Init();
+  MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
   HAL_TIM_IC_Start_IT(&htim1, TIM_CHANNEL_1);
+  HAL_TIM_IC_Start_IT(&htim2, TIM_CHANNEL_4);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -159,6 +317,7 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+
 	HCSR04_Read();
 	HAL_Delay(1000);
 	if(Distance<10){
@@ -169,6 +328,7 @@ int main(void)
 		HAL_GPIO_WritePin(GPIOD,GPIO_PIN_15,0);
 		HAL_GPIO_WritePin(GPIOD,GPIO_PIN_13,1);
 	}
+	garagecontrol();
 
   }
   /* USER CODE END 3 */
@@ -268,6 +428,65 @@ static void MX_TIM1_Init(void)
 }
 
 /**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 72-1;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 2000;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 500;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
+  HAL_TIM_MspPostInit(&htim2);
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -281,12 +500,22 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOE_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13|GPIO_PIN_15, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PA0 PA1 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PA2 */
   GPIO_InitStruct.Pin = GPIO_PIN_2;
@@ -301,6 +530,20 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PC9 */
+  GPIO_InitStruct.Pin = GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI0_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI0_IRQn);
+
+  HAL_NVIC_SetPriority(EXTI1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI1_IRQn);
 
 }
 
