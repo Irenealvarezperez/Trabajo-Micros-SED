d8c87077794d53a34f1047989cb2f1c0ffc1a245
Irenealvarezperez <irene.alvarez.perez@alumnos.upm.es>
2022-01-02 20:29:40 +0100

    .

--- Trabajo_SED_Micros/Core/Src/freertos.c
+++ Trabajo_SED_Micros/Core/Src/freertos.c
@@ -57,4 +57,15 @@
 /* USER CODE BEGIN Application */
 
 /* USER CODE END Application */
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize );
 
+/* USER CODE BEGIN GET_IDLE_TASK_MEMORY */
+static StaticTask_t xIdleTaskTCBBuffer;
+static StackType_t xIdleStack[configMINIMAL_STACK_SIZE];
+
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize )
+{
+  *ppxIdleTaskTCBBuffer = &xIdleTaskTCBBuffer;
+  *ppxIdleTaskStackBuffer = &xIdleStack[0];
+  *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+}
